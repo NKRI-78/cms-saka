b22e3a821ef84bc91299674a1875278f1d690239
Rangga adi putra <ranggadiputra23@gmail.com>
2024-09-11 13:43:13 +0700

    update

--- app/Controllers/Admin/OfficialStoreController.php
+++ app/Controllers/Admin/OfficialStoreController.php
@@ -52,7 +52,7 @@ class OfficialStoreController extends BaseController
                 $resultSubistrict = curlHelper(getenv('ECOMMERCE_URL') . '/ecommerces/v1/regions/subdistrict/' . $district, 'GET');
 
                 $data["province"] = $resultProvince->data;
-                $data["City"] = $resultCity->data;
+                $data["kota"] = $resultCity->data;
                 $data["district"] = $resultDistrict->data;
                 $data["subdistrict"] = $resultSubistrict->data;
             }
