89322d331c49aa8762eb3187e234a72f1f27965a
Rangga adi putra <ranggadiputra23@gmail.com>
2024-09-23 14:33:41 +0700

    update dashboard revenue

--- app/Controllers/Admin/DashboardController.php
+++ app/Controllers/Admin/DashboardController.php
@@ -9,6 +9,58 @@ class DashboardController extends BaseController
 {
   public function index()
   {
-    return view("admin/dashboard/index");
+    $client = new \GuzzleHttp\Client();
+    $session = Services::session();
+    $request = Services::request();
+
+    $session = session();
+    $user_id = $session->get('userId');
+
+    $postData = [
+      'user_id' => $user_id
+      // 'user_id' => '61f19d24-3826-4f6d-8c80-121da8a2fc3a'
+    ];
+
+    $url = getenv('ECOMMERCE_URL') . '/ecommerces/v1/stores';
+
+    $response = $client->post(
+      $url,
+      [
+        "body" => json_encode($postData),
+        'headers' =>  [
+          'Authorization' => 'Bearer ' . $session->get('token'),
+          'Accept'        => 'application/json',
+          'Content-Type'  => 'application/json',
+        ]
+      ]
+    );
+
+    $resultStore = json_decode($response->getBody(), true);
+
+    $storeId = $resultStore['data']['id'];
+
+    $body = [
+      'store_id' => $storeId
+    ];
+
+    $url = getenv('ECOMMERCE_URL') . '/ecommerces/v1/stores/revenue';
+
+    $responseRevenue = $client->post(
+      $url,
+      [
+        "body" => json_encode($body),
+        'headers' =>  [
+          'Authorization' => 'Bearer ' . $session->get('token'),
+          'Accept'        => 'application/json',
+          'Content-Type'  => 'application/json',
+        ]
+      ]
+    );
+
+    $resultRevenue = json_decode($responseRevenue->getBody(), true);
+
+    $data["revenue"] = $resultRevenue['data'];
+
+    return view("admin/dashboard/index", $data);
   }
 }
