5a142873e6a2952092067f503a0f9a21fd5b874b
Rangga adi putra <ranggadiputra23@gmail.com>
2024-10-02 11:04:29 +0700

    update kondisi dashboard untuk role admin tidak tampil

--- app/Controllers/Admin/DashboardController.php
+++ app/Controllers/Admin/DashboardController.php
@@ -9,57 +9,67 @@ class DashboardController extends BaseController
 {
   public function index()
   {
-    $client = new \GuzzleHttp\Client();
-    $session = Services::session();
-    $request = Services::request();
-
     $session = session();
-    $user_id = $session->get('userId');
+    $data = [];
 
-    $postData = [
-      'user_id' => $user_id
-      // 'user_id' => '61f19d24-3826-4f6d-8c80-121da8a2fc3a'
-    ];
+    if ($session->get('role') !== 'admin') {
+      $client = new \GuzzleHttp\Client();
+      $session = Services::session();
+      $request = Services::request();
 
-    $url = getenv('ECOMMERCE_URL') . '/ecommerces/v1/stores';
+      $user_id = $session->get('userId');
 
-    $response = $client->post(
-      $url,
-      [
-        "body" => json_encode($postData),
-        'headers' =>  [
-          'Authorization' => 'Bearer ' . $session->get('token'),
-          'Accept'        => 'application/json',
-          'Content-Type'  => 'application/json',
+      $postData = [
+        'user_id' => $user_id
+        // 'user_id' => '61f19d24-3826-4f6d-8c80-121da8a2fc3a'
+      ];
+
+      $url = getenv('ECOMMERCE_URL') . '/ecommerces/v1/stores';
+
+      $response = $client->post(
+        $url,
+        [
+          "body" => json_encode($postData),
+          'headers' =>  [
+            'Authorization' => 'Bearer ' . $session->get('token'),
+            'Accept'        => 'application/json',
+            'Content-Type'  => 'application/json',
+          ]
         ]
-      ]
-    );
+      );
 
-    $resultStore = json_decode($response->getBody(), true);
+      $resultStore = json_decode($response->getBody(), true);
 
-    $storeId = $resultStore['data']['id'];
+      if (isset($resultStore['data']['id'])) {
+        $storeId = $resultStore['data']['id'];
 
-    $body = [
-      'store_id' => $storeId
-    ];
+        $body = [
+          'store_id' => $storeId
+        ];
 
-    $url = getenv('ECOMMERCE_URL') . '/ecommerces/v1/stores/revenue';
+        $url = getenv('ECOMMERCE_URL') . '/ecommerces/v1/stores/revenue';
 
-    $responseRevenue = $client->post(
-      $url,
-      [
-        "body" => json_encode($body),
-        'headers' =>  [
-          'Authorization' => 'Bearer ' . $session->get('token'),
-          'Accept'        => 'application/json',
-          'Content-Type'  => 'application/json',
-        ]
-      ]
-    );
+        $responseRevenue = $client->post(
+          $url,
+          [
+            "body" => json_encode($body),
+            'headers' =>  [
+              'Authorization' => 'Bearer ' . $session->get('token'),
+              'Accept'        => 'application/json',
+              'Content-Type'  => 'application/json',
+            ]
+          ]
+        );
 
-    $resultRevenue = json_decode($responseRevenue->getBody(), true);
+        $resultRevenue = json_decode($responseRevenue->getBody(), true);
 
-    $data["revenue"] = $resultRevenue['data'];
+        $data["revenue"] = $resultRevenue['data'];
+      } else {
+        // Jika store tidak ditemukan, beri pesan error
+        $data["revenue"] = ['0'];
+        $data["message"] = "Store tidak ditemukan";
+      }
+    }
 
     return view("admin/dashboard/index", $data);
   }
